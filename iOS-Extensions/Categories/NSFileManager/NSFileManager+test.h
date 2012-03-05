9c7b456d1c7ea5c63bcefcb9065bad21a7f4e651
Itzak Tasi <ItzakTasi@gmail.com>
2012-03-05 13:51:48 +0800

    Test Commit

---
+++ iOS-Extensions/Categories/NSFileManager/NSFileManager+test.h
@@ -0,0 +1,13 @@
+//
+//  NSFileManager+test.h
+//  iOS-Extensions
+//
+//  Created by Itzak Tasi on 12/3/5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFileManager (test)
+
+@end
