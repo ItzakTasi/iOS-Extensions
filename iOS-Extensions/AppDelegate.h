546f2a4b281e943a7b246f7c786ccbe26b53f0ef
Itzak Tasi <ItzakTasi@gmail.com>
2012-03-05 13:42:21 +0800

    Initial Commit

---
+++ iOS-Extensions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Extensions
+//
+//  Created by Itzak Tasi on 12/3/5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-Extensions/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS-Extensions
+//
+//  Created by Itzak Tasi on 12/3/5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
